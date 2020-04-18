2b8cab1a2b97aaa70ccf5b337172d7a157b48d6b
David Li <david.li3100@gmail.com>
2020-04-18 19:31:26 -0400

    minor scaffolding updates

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+admin.initializeApp();
 
-admin.initalizeApp();
 
-exports.vote() = functions.https.onRequest(async (req, res) => {
+exports.vote = functions.https.onRequest(async (req, res) => {
     let votes = req.body.votes;
+    console.log(req.body.votes)
+    
+    //do something and then return a 201
+    res.status(201).send()
 })
