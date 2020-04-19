65d1fe182e4f6fbfaed213c2f5a1f0a19f735ecb
David Li <david.li3100@gmail.com>
2020-04-18 21:12:48 -0400

    basic voting api created

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,31 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+admin.initializeApp({
+    credential: admin.credential.cert(require("../fraser-votes-firebase-adminsdk-tnue9-356f4872d6.json"))
+});
 
 
-exports.vote = functions.https.onRequest(async (req, res) => {
-    let votes = req.body.votes;
-    console.log(req.body.votes)
-    
-    //do something and then return a 201
-    res.status(201).send()
+exports.vote = functions.https.onCall(async (data, context) => {
+    let votes = data.votes;
+    console.log(data.votes)
+    console.log(context.auth)
+
+    return admin.firestore().collection("users").doc(context.auth.token.email.split("@")[0]).get().then(docSnapshot => {
+        if (docSnapshot.exists) {
+            let userData = docSnapshot.data()
+            console.log(userData)
+            if (!userData.voted) {
+                admin.firestore().collection("ballots").add({
+                    date:  admin.firestore.FieldValue.serverTimestamp(),
+                    userGPG: context.auth.token.uid,
+                    votes: data.votes
+                })
+            } else {    
+                throw new functions.https.HttpsError("unauthorized operation", "this user has already voted")
+            }
+            return true
+        } else {
+            throw new functions.https.HttpsError("unauthorized operation", "this user is not authorized to perform this operation")
+        }
+    })
 })
